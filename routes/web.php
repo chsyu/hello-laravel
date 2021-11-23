1c196d376e80858873c5d130cba3e2bd0a0425c2
chsyu <chsyu@tea.ntue.edu.tw>
2021-11-23 09:58:51 +0800

    add api

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "<h1>Hello World</h1>";
+});
+
+
+Route::get('/date/{date1}/{date2}', function($date1, $date2) {
+    return "Date1 = ". $date1. ", and Date2 = ". $date2;
+});
+
+
+Route::get('/user/info', function() {
+    $name = 'abc';
+    $email = "abc@gmail.com";
+    return response()->json([
+        'name' => $name,
+        'email' => $email,
+    ]);
 });
